eb17d51d6afd5a43307a1f5f2313ff0a84c95d43
Aniruddha Shriwant <aniruddhashriwant@gmail.com>
2021-01-28 19:54:39 +0530

    Added Python Practical docs n codes
    Signed-off-by: Aniruddha Shriwant <aniruddhashriwant@gmail.com>

---
+++ Python/College Practicals/fs31/codes/exp10.py
@@ -0,0 +1,8 @@
+name=input('enter your name : ')
+contact_no=input('enter your contact no : ')
+percentage=float(input('enter your percentage : '))
+
+print('name is : ',name)
+print('contact no is : ',contact_no)
+print('percentages are : ',percentage)
+
---
+++ Python/College Practicals/fs31/codes/exp3_c.py
@@ -0,0 +1,14 @@
+import cmath
+
+a = 1
+b = 5
+c = 6
+
+# calculate the discriminant
+d = (b**2) - (4*a*c)
+
+# find two solutions
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+print(sol1, ' and ',sol2,'are the roots of the given quadratic euquation')
